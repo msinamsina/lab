8c4d5e18bbe8361f9cf734ca15444e77fa6516df
Mohammad sina Allahkaram <msinamsina@gmail.com>
2025-03-20 15:27:35 +0000

    feat: add simple script for installing docker

---
+++ Docker/install-docker-and-docker-compose.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo apt update
+sudo apt install docker-ce
+sudo systemctl status docker
+sudo usermod -aG docker ${USER}
+su - ${USER}
