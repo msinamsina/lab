7ce9f68cbb04d97e139bd36bed0304f656cfc130
Mohammad sina Allahkaram <msinamsina@gmail.com>
2025-03-20 15:34:07 +0000

    fix: update script to skip approve for instalation

--- Docker/install-docker-and-docker-compose.sh
+++ Docker/install-docker-and-docker-compose.sh
@@ -1,8 +1,8 @@
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 sudo apt update
-sudo apt install docker-ce
+sudo apt install docker-ce -y
 sudo systemctl status docker
 sudo usermod -aG docker ${USER}
 su - ${USER}
